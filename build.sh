c034d5bb6dea98facedcd9abdf829055a0f78995
lizeen <lizeen@baidu.com>
2022-04-19 18:49:54 +0800

    添加build.sh构建脚本

---
+++ build.sh
@@ -0,0 +1,7 @@
+npm install
+cd ui && npm run build && cp dist/index.html dist/static/index.html
+cd ..
+export GOPROXY=https://proxy.golang.com.cn,direct
+go get github.com/jessevdk/go-assets-builder
+go-assets-builder -s ui/dist/static ui/dist -o pkg/router/assets.go -p router
+make build-binary
