dbf31ede00e70bdca627df8ba2955741ba1c12a8
ouxl <ouxl>
2022-04-19 22:03:38 +0800

    更新build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
-cd ui && npm install
-npm run build && cp dist/index.html dist/static/index.html
+cd ui
+npm config set registry https://registry.npm.taobao.org
+npm install
+npm run build
+cp dist/index.html dist/static/index.html
 cd ..
 export GOPROXY=https://proxy.golang.com.cn,direct
 go get github.com/jessevdk/go-assets-builder
-go-assets-builder -s ui/dist/static ui/dist -o pkg/router/assets.go -p router
+go-assets-builder -s /ui/dist/static ui/dist -o pkg/router/assets.go -p router
 make build-binary
