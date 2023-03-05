d570a62372cf2ace99873cca3f1c16196170ea47
jmszg <1243541405@qq.com>
2023-03-05 15:03:35 +0800

    delete file

--- build/amd64/build-images.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-docker build -t registry.cn-hangzhou.aliyuncs.com/zgjhub/kubespace-controller:1.1.2 -f Dockerfile_controller .
-docker build -t registry.cn-hangzhou.aliyuncs.com/zgjhub/kube-agent:1.1.2 -f Dockerfile_kubeagent .
-docker build -t registry.cn-hangzhou.aliyuncs.com/zgjhub/kubespace-server:1.1.2 -f Dockerfile_server .
-
-docker push registry.cn-hangzhou.aliyuncs.com/zgjhub/kubespace-controller:1.1.2
-docker push registry.cn-hangzhou.aliyuncs.com/zgjhub/kube-agent:1.1.2
-docker push registry.cn-hangzhou.aliyuncs.com/zgjhub/kubespace-server:1.1.2
