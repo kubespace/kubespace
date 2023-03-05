880540b542b73741e3e8f8cc4aa3cb8ac203426c
jmszg <1243541405@qq.com>
2023-03-05 15:59:49 +0800

    update

--- cmd/controller-manager/main.go
+++ cmd/controller-manager/main.go
@@ -10,15 +10,15 @@ import (
 )
 
 var (
-	redisAddress  = flag.String("redis-address", utils.LookupEnvOrString("REDIS_ADDRESS", "192.168.47.150:6379"), "redis address used.")
+	redisAddress  = flag.String("redis-address", utils.LookupEnvOrString("REDIS_ADDRESS", "localhost:6379"), "redis address used.")
 	redisDB       = flag.Int("redis-db", utils.LookupEnvOrInt("REDIS_DB", 0), "redis db used.")
-	redisPassword = flag.String("redis-password", utils.LookupEnvOrString("REDIS_PASSWORD", ""), "redis password used.")
-	mysqlHost     = flag.String("mysql-host", utils.LookupEnvOrString("MYSQL_HOST", "192.168.47.150:3306"), "mysql address used.")
+	redisPassword = flag.String("redis-password", utils.LookupEnvOrString("REDIS_PASSWORD", "123abc,.;"), "redis password used.")
+	mysqlHost     = flag.String("mysql-host", utils.LookupEnvOrString("MYSQL_HOST", "localhost:3306"), "mysql address used.")
 	mysqlUser     = flag.String("mysql-user", utils.LookupEnvOrString("MYSQL_USER", "root"), "mysql db user.")
-	mysqlPassword = flag.String("mysql-password", utils.LookupEnvOrString("MYSQL_PASSWORD", "root"), "mysql password used.")
+	mysqlPassword = flag.String("mysql-password", utils.LookupEnvOrString("MYSQL_PASSWORD", ""), "mysql password used.")
 	mysqlDbName   = flag.String("mysql-dbname", utils.LookupEnvOrString("MYSQL_DBNAME", "kubespace"), "mysql db used.")
 	resyncSec     = flag.Int("resync-seconds", utils.LookupEnvOrInt("RESYNC_SECONDS", 5), "controller list resync seconds.")
-	dataDir       = flag.String("data-dir", utils.LookupEnvOrString("DATA_DIR", "data"), "data directory.")
+	dataDir       = flag.String("data-dir", utils.LookupEnvOrString("DATA_DIR", "/data"), "data directory.")
 )
 
 func main() {
